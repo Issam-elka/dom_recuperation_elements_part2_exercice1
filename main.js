0e4ca51910cb27a0302983c0462ce0efb09ed1e4
Issam-elka <80962666+Issam-elka@users.noreply.github.com>
2021-05-12 10:21:31 +0200

    DOM_exo2prt1

---
+++ main.js
@@ -0,0 +1,13 @@
+//#1
+let exo1 = document.getElementsByClassName('tartine');
+console.log(exo1);
+
+//#2
+let exo2 = document.getElementById('toast');
+console.log(exo2);
+
+//#3
+let exo3 = document.getElementsByTagName('body')[0];
+let exo33 = exo3.getElementsByClassName('tartine')
+console.log(exo3);
+console.log(exo33);
